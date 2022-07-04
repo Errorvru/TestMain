b0b5fce4eb7dc1229560f97ab2fb07f07e5eac62
Errorvru <vrushabhjain740@gmail.com>
2022-07-04 17:33:29 +0530

    Test

---
+++ TestMain.java
@@ -0,0 +1,15 @@
+package com.app.javaProject.JavaPro;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+	@Test
+	public void testInputIsEven() {
+		assertTrue(Main.checkIfInputIsAnEvenNumber(23)); // Assertion
+	}
+
+	private void assertTrue(boolean checkIfInputIsAnEvenNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+}
